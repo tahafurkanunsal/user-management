6314529ff94573ab5daf5eb9b662e5370d41514e
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-10-03 20:48:53 +0300

    The updated entity class has been loaded.

--- src/main/java/com/SpringBoot/SpringBootDatabaseProject/entities/User.java
+++ src/main/java/com/SpringBoot/SpringBootDatabaseProject/entities/User.java
@@ -1,6 +1,12 @@
 package com.SpringBoot.SpringBootDatabaseProject.entities;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,7 +14,11 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "User")
-
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
@@ -17,35 +27,4 @@ public class User {
     private String lastName;
     private String email;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
