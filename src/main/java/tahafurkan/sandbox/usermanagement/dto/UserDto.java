49f53b6ab4889ebfbf14429d92c94aaf8d445d85
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-10-23 13:08:51 +0300

    model mapper configurasyonu yapıldı ve user dto sınıfı eklendi

---
+++ src/main/java/tahafurkan/sandbox/usermanagement/dto/UserDto.java
@@ -0,0 +1,13 @@
+package tahafurkan.sandbox.usermanagement.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String username;
+
+}
