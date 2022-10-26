6e1909816fbdc9a98c085b9c0f2ce9251e44d628
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-10-26 18:12:29 +0300

    address alanı eklendi.

---
+++ src/main/java/tahafurkan/sandbox/usermanagement/entities/Address.java
@@ -0,0 +1,24 @@
+package tahafurkan.sandbox.usermanagement.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity(name = "addresses")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String addressName;
+    private String city;
+    private String district;
+    @ManyToOne
+    @JoinColumn( referencedColumnName = "id")
+    private User user;
+
+}
---
+++ src/main/java/tahafurkan/sandbox/usermanagement/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package tahafurkan.sandbox.usermanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tahafurkan.sandbox.usermanagement.entities.Address;
+
+public interface AddressRepository extends JpaRepository<Address , Integer> {
+}
