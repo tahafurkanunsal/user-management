e25a5fdb475cefacd23dc464c5265d84791514e1
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-10-29 00:28:26 +0300

    address alanları userDto alanlarına donusturuldu

--- src/main/java/tahafurkan/sandbox/usermanagement/dto/UserConverter.java
+++ src/main/java/tahafurkan/sandbox/usermanagement/dto/UserConverter.java
@@ -1,4 +1,31 @@
 package tahafurkan.sandbox.usermanagement.dto;
 
-public class Converter {
+import tahafurkan.sandbox.usermanagement.entities.Address;
+import tahafurkan.sandbox.usermanagement.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserConverter {
+    public UserDto toUserDTo(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setEmail(user.getEmail());
+        userDto.setName(user.getName());
+        userDto.setLastName(user.getLastName());
+
+
+        List<Address> addresses = user.getAddress();
+
+        List<AddressDto> addressDtoList = new ArrayList<>();
+        for(Address address : addresses) {
+            AddressDto addressDto = new AddressDto();
+            addressDto.setCity(address.getCity());
+           addressDto.setDistrict(address.getDistrict());
+           addressDto.setAddressName(address.getAddressName());
+            addressDtoList.add(addressDto);
+        }
+        userDto.setAddress(addresses);
+        return userDto;
+    }
 }
+
