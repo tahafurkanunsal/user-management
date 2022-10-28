d5abea6fb0d43b23db0fb96cc07a1d344983d0f6
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-10-29 00:19:04 +0300

    address dto sınıfı eklendi

---
+++ src/main/java/tahafurkan/sandbox/usermanagement/dto/AddressDto.java
@@ -0,0 +1,14 @@
+package tahafurkan.sandbox.usermanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDto {
+    private String addressName;
+    private String city;
+    private String district;
+}
---
+++ src/main/java/tahafurkan/sandbox/usermanagement/dto/UserConverter.java
@@ -0,0 +1,4 @@
+package tahafurkan.sandbox.usermanagement.dto;
+
+public class Converter {
+}
