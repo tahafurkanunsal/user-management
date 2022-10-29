da3b88be50685fb09f981ed0871e105a57d16bee
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-10-29 17:03:22 +0300

    address converter sınıfı olusturuldu

---
+++ src/main/java/tahafurkan/sandbox/usermanagement/dto/AddressConverter.java
@@ -0,0 +1,21 @@
+package tahafurkan.sandbox.usermanagement.dto;
+
+import tahafurkan.sandbox.usermanagement.entities.Address;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressConverter {
+    public void toAddressDto(Address address) {
+
+        List<AddressDto> addressDtoList = new ArrayList<>();
+
+            AddressDto addressDto = new AddressDto();
+            addressDto.setCity(address.getCity());
+            addressDto.setDistrict(address.getDistrict());
+            addressDto.setAddressName(address.getAddressName());
+            addressDtoList.add(addressDto);
+
+    }
+    }
+
