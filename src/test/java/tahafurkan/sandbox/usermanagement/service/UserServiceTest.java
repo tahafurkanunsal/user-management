7895f301ef66491ed9c5e455e613531b54759718
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-11-04 17:25:05 +0300

    checkUsername metodu icin unit test yazıldı

--- src/test/java/tahafurkan/sandbox/usermanagement/service/UserServiceTest.java
+++ src/test/java/tahafurkan/sandbox/usermanagement/service/UserServiceTest.java
@@ -8,7 +8,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tahafurkan.sandbox.usermanagement.entities.User;
+import tahafurkan.sandbox.usermanagement.exception.UsernameIsInUseException;
+import tahafurkan.sandbox.usermanagement.exception.UsernameUnavailableException;
 import tahafurkan.sandbox.usermanagement.repository.UserRepository;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -18,13 +23,19 @@ class UserServiceTest {
 
     @InjectMocks
     UserService userService;
+    @Test
+    void checkUsername() throws UsernameUnavailableException, UsernameIsInUseException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method =UserService.class.getDeclaredMethod("checkUsername", String.class);
+        method.setAccessible(true);
+        method.invoke(userService , "Joker");
+    }
 
     @Test
     void create(){
         User user = new User();
         user.setUsername("Joker");
         Mockito.when(userRepository.save(user)).thenReturn(user);
-        Assertions.assertEquals(user,userService.create(user));
+        Assertions.assertEquals(user, userService.create(user));
     }
 
 }
