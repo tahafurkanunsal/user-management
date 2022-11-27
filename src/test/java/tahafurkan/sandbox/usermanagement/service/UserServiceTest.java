1945930ddb7f7e0c10877004e01549dc34d46074
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-11-27 20:29:00 +0300

    issue-34 create metodu için unit test yazıldı.

--- src/test/java/tahafurkan/sandbox/usermanagement/service/UserServiceTest.java
+++ src/test/java/tahafurkan/sandbox/usermanagement/service/UserServiceTest.java
@@ -1,19 +1,20 @@
 package tahafurkan.sandbox.usermanagement.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tahafurkan.sandbox.usermanagement.entities.Address;
 import tahafurkan.sandbox.usermanagement.entities.User;
-import tahafurkan.sandbox.usermanagement.exception.UsernameIsInUseException;
-import tahafurkan.sandbox.usermanagement.exception.UsernameUnavailableException;
 import tahafurkan.sandbox.usermanagement.repository.UserRepository;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Collections;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -21,30 +22,43 @@ class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
-
     @InjectMocks
     UserService userService;
-
     @Test
-    void shouldCheckTheAvailabilityOfTheUsername() throws UsernameUnavailableException, UsernameIsInUseException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    void shouldCheckTheAvailabilityOfTheUsername() throws Throwable {
         Method method = UserService.class.getDeclaredMethod("checkUsername", String.class);
         method.setAccessible(true);
-        method.invoke(userService, "Joker");
+        String username = "Dummy";
+        User user = new User();
+        user.setUsername(username);
+
+        User availability = userService.create(user);
+        assertThat(availability);
+
     }
-
     @Test
-    void shouldCheckIfUsernameExists() throws Throwable {
+    void shouldCheckIfUsernameExists() throws Throwable{
         Method method = UserService.class.getDeclaredMethod("checkUsername", String.class);
         method.setAccessible(true);
-        method.invoke(userService, "Joker");
+        String username = "Joker";
+        User user = new User();
+        user.setUsername(username);
+
+        boolean exists = userRepository.existsByUsername(username);
+        assertThat(exists);
     }
 
     @Test
     void create() {
-        User user = new User();
-        user.setUsername("Joker");
+        User user = new User(1,
+                "Taha Furkan",
+                "Unsal",
+                "t.furkanunsal.gmail.com",
+                "TFU",
+                Collections.singletonList(new Address(1, "Home", "Istanbul", "Bahcelievler")));
         Mockito.when(userRepository.save(user)).thenReturn(user);
-        Assertions.assertEquals(user, userService.create(user));
-    }
+        assertThat(userService.create(user));
 
+
+    }
 }
