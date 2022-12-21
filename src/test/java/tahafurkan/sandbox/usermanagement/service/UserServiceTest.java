4dbf860cbfc60ae0604465901f7ffd8d9d14827d
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-12-21 20:47:36 +0300

    Updated the unit test cases in the user service.

--- src/test/java/tahafurkan/sandbox/usermanagement/service/UserServiceTest.java
+++ src/test/java/tahafurkan/sandbox/usermanagement/service/UserServiceTest.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
@@ -31,6 +32,7 @@ class UserServiceTest {
 
     @Spy
     private UserRepository userRepository;
+
     @Spy
     private ModelMapper modelMapper;
 
@@ -101,8 +103,9 @@ class UserServiceTest {
         Mockito.verify(userRepository, times(1)).save(user);
     }
 
+
     @Test
-    void get_GivenIdFind_ThrowsException() {
+    void get_WithAnIdNotExist_ThrowsException() {
         assertThrows(NoSuchUserExistsException.class, () -> {
             int id = 1;
             User user = new User();
@@ -112,38 +115,88 @@ class UserServiceTest {
     }
 
     @Test
-    void getAll_ReturnsUser() {
+    void get_WithAnIdExists_ReturnUser() {
+        int id = 1;
+        User user = new User();
+        user.setId(id);
+        given(userRepository.findById(id)).willReturn(Optional.of(user));
+        userService.get(id);
+    }
+
+    @Test
+    void getAll_ReturnsEmptyUserList() {
         userService.getAll();
-        Mockito.verify(userRepository).findAll();
+        Mockito.verify(userRepository, times(1)).findAll();
     }
 
     @Test
-    void delete_GivenIdDelete_ThrowsException() {
-        assertThrows(NoSuchUserExistsException.class, () -> {
-            int id = 1;
-            userService.delete(id);
-        });
+    void getAll_ReturnsPresentUserList() {
+        List<User> records = new ArrayList<>();
+
+        List<Address> addressList = new ArrayList<>();
+        addressList.add(new Address(1, "home", "Istanbul", "Turkey"));
+        User user_1 = new User(1, "Taha", "Unsal", "taha.f.unsal@gmail.com","taha.furkan",  addressList);
+        User user_2 = new User(2, "Zehra", "Unsal", "zehra.unsal@gmail.com","zehra.unsal", addressList);
+        records.add(user_1);
+        records.add(user_2);
+
+        given(userRepository.findAll()).willReturn(records);
+
+        Mockito.verify(userRepository , verificationData -> records.size());
+        
     }
 
     @Test
-    void getByUsername_GivenFindUsername_ReturnUser() {
+    void delete_WithNonExistentId_ThrowsException() {
+
+    }
+
+    @Test
+    void delete_WithExistentId_ThrowsException() {
+        int id = 1;
+        User user = new User();
+        user.setId(id);
+        given(userRepository.findById(id)).willReturn(Optional.of(user));
+        userService.delete(id);
+
+
+    }
+
+    @Test
+    void getByUsername_GivenUsernameExists_ReturnsUser() {
         String username = "taha.furkan";
         User user = new User();
         user.setUsername(username);
         given(userRepository.findByUsername(username)).willReturn(user);
         userService.getByUsername(username);
+        assertEquals(user.getUsername(), username);
     }
+
     @Test
-    void update_GivenIdUpdate_ReturnUser(){
-        int id = 1;
-        User existsUser = new User();
-        existsUser.setId(id);
-        given(userRepository.findById(id)).willReturn(Optional.of(existsUser));
+    void getByUsername_GivenUsernameNonExists_ThrowsException() {
+        String username = "taha.furkan";
+        User user = new User();
+        user.setUsername(username);
+        userService.getByUsername(username);
+    }
+
+
+    @Test
+    void update_UpdatesLastName_ReturnsUpdatedUser() {
         List<Address> addressList = new ArrayList<>();
         addressList.add(new Address(1, "home", "Istanbul", "Turkey"));
-        addressList.add(new Address(2, "work", "Istanbul", "Turkey"));
-        UserDto user = new UserDto(1, "Taha", "Unsal", "taha.f.unsal@gmail.com", addressList);
-        userService.update(id , user);
+        User user = new User(1, "Taha", "Unsal", "taha.f.unsal@gmail.com", "taha.furkan", addressList);
+
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+
+        UserDto updateUser = new UserDto();
+        updateUser.setLastName("furkan");
+        user.setLastName(updateUser.getLastName());
+
+        given(userRepository.save(user)).willReturn(user);
+
+        userService.update(user.getId(), updateUser);
+
 
     }
 
